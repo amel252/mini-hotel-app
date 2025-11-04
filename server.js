f1339f8ac7cc87f509745cf6b9a4c9b44abc8361
Amel252 <amelghanem25@gmail.com>
2025-11-04 02:16:09 +0100

    config de notre BD avec notre mongoose

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
 import routes from "./routes/routes.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3200;
+
 //instancié express
 const app = express();
+
 // parametrer notre server pour lire json
 app.use(express.json());
+
+// connextion mongoDb
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB est connecté ✅"))
+    .catch((err) => console.error("Erreur de connexion MongoDB ❌", err));
+
 app.use(routes);
 
 app.listen(PORT, () => {
