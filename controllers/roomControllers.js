caf82582326c98df50c5932167c30e75fee97cab
Amel252 <amelghanem25@gmail.com>
2025-11-05 00:35:55 +0100

    addRoom (creation de la chambre ) crud partie 1

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,7 +1,23 @@
+import RoomModel from "../models/roomModel.js";
+
 export const getTest = (req, res) => {
     res.send("Hello les amis ");
 };
 
 export const postTest = (req, res) => {
     res.send(req.body);
+};
+
+// create a room
+export const addRoom = async (req, res) => {
+    // On crée un nouvel objet room à partir du modèle RoomModel.
+    const room = new RoomModel(req.body);
+    try {
+        // On essaye d’enregistrer la nouvelle chambre dans la BD
+        await room.save();
+        res.send(room);
+        // Si ok on renvoie la chambre enregistrée au client
+    } catch (error) {
+        res.status(500).send(error);
+    }
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getTest, postTest } from "../controllers/roomControllers.js";
+import { getTest, postTest, addRoom } from "../controllers/roomControllers.js";
 
 const router = express.Router();
 
 router.get("/", getTest);
 router.post("/test", postTest);
+router.post("/add-room", addRoom);
 
 export default router;
