9d8dbeddb76ca963a6fb9ce555efcd066c7e17fd
Amel252 <amelghanem25@gmail.com>
2025-11-10 00:35:07 +0100

    Réorganisation projet ( i antd , router-dom)

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,11 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
-    return <></>;
-}
+const App = () => {
+    return (
+        <>
+            <h1>Home hotel site</h1>
+        </>
+    );
+};
 
 export default App;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "antd/dist/reset.css";
+import App from "./App.jsx";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </StrictMode>
+);
