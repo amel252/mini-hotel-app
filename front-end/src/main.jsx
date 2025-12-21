97232408ba9a5e87dc5a0c3c86fee14308fb6c90
Amel252 <amelghanem25@gmail.com>
2025-12-22 00:58:32 +0100

    installation redux toolkit et modification de main

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,30 +1,11 @@
-// import React from "react";
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "bootstrap/dist/css/bootstrap.min.css";
-
-// import "antd/dist/reset.css";
-// import App from "./App.jsx";
-
-// import { BrowserRouter as Router } from "react-router-dom";
-
-// createRoot(document.getElementById("root")).render(
-//     <StrictMode>
-//         <Router>
-//             <App />
-//         </Router>
-//     </StrictMode>
-// );
-// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    //<React.StrictMode> // <-- commente temporairement si ça double les effets
-    <BrowserRouter>
+    <Provider store={store}>
         <App />
-    </BrowserRouter>
-    //</React.StrictMode>
+    </Provider>
 );
