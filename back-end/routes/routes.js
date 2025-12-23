c8287d52e55a7d088942453bf75618963902d631
Amel252 <amelghanem25@gmail.com>
2025-12-23 13:25:50 +0100

    Correction + ajout de CatchErrors dans mes routes

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -12,9 +12,9 @@ const router = express.Router();
 
 router.post("/api/rooms", addRoom); // Créer une chambre
 router.get("/api/rooms", catchErrors(getRooms)); // obtenir toute les chambres
-router.get("/api/rooms/:id", getRoomById); // Obtenir une chambre
-router.patch("/api/rooms/:id", updateRoom); // Mettre à jour partiellement
-router.put("/api/rooms/:id", updateRoom); // Mettre à jour une chambre
-router.delete("/api/rooms/:id", deleteRoom); // Supprimer une chambre
+router.get("/api/rooms/:id", catchErrors(getRoomById)); // Obtenir une chambre
+router.patch("/api/rooms/:id", catchErrors(updateRoom)); // Mettre à jour partiellement
+router.put("/api/rooms/:id", catchErrors(updateRoom)); // Mettre à jour une chambre
+router.delete("/api/rooms/:id", catchErrors(deleteRoom)); // Supprimer une chambre
 
 export default router;
--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -9,3 +9,11 @@ export default {
         },
     },
 };
+// Remarques:
+//  quand je fait npm run build au terminal (front-end) Vite fait le ménage et prépare le site pour Internet=> vite Analyse ton code React -> Trouve les images importées -> Les copie dans dist/assets /assets->  Les renomme avec un hash  -> Génère index.html, JS, CSS  ➡️ Tout ça se passe AVANT Express
+
+//  quand express démarre -> express voit cette ligne -> app.use(express.static("front-end/dist"))=> Il se dit : “Si quelqu’un demande un fichier, je vais le chercher dans dist et je le renvoie.”
+
+//  Quand un visiteur ouvre le site => Le navigateur demande :http://monsite.com/ =>Express : envoie dist/index.html => Puis le navigateur demande : du js ; css, des imgs
+
+// => Express : les lit dans dist/assets => les envoie => Express ne crée rien, il distribue juste
